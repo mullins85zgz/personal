36b691fc0a37ae32893ef630971fbb22810028b1
salinjos <joseangel.salinas@hpe.com>
2015-10-07 09:51:01 +0200

    Versión inicial

---
+++ PruebasGIT/src/es/jasalinas/Prueba.java
@@ -0,0 +1,13 @@
+package es.jasalinas;
+
+public class Prueba {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo de GIT!!!");
+	}
+
+}
