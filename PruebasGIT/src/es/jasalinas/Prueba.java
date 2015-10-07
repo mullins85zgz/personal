dc816cffa9ea2c1fcb1933fbf5e2678dff76a838
salinjos <joseangel.salinas@hpe.com>
2015-10-07 12:34:14 +0200

    Cambio desde repositorio B

--- PruebasGIT/src/es/jasalinas/Prueba.java
+++ PruebasGIT/src/es/jasalinas/Prueba.java
@@ -7,7 +7,7 @@ public class Prueba {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo de GIT!!! Version 2");
+		System.out.println("Hola Mundo de GIT!!! Version 3, en repositorio B");
 	}
 
 }
